9acebba4640f5918eed201067fdf33bfe8d7c421
DengreSarthak <dengresathak2002@gmail.com>
2024-12-29 22:50:35 +0530

    rest

--- next.config.mjs
+++ next.config.mjs
@@ -7,8 +7,6 @@ const nextConfig = {
     unoptimized: true,
   },
   basePath: '/hodlCoin-Solidity-WebUI',
-  assetPrefix: '/hodlCoin-Solidity-WebUI',
-  // trailingSlash: true,
 }
 
 export default nextConfig;
