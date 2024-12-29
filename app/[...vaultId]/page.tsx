f9f1b4e0a2d3edb3a3f20720fbf433195f3281a7
DengreSarthak <dengresathak2002@gmail.com>
2024-12-29 23:08:12 +0530

    test

---
+++ app/[...vaultId]/page.tsx
@@ -0,0 +1,21 @@
+import { notFound } from 'next/navigation'
+import InteractionClient from './InteractionClient'
+
+export async function generateStaticParams() {
+  return [{ vaultId: ['v'] }]
+}
+
+export default function VaultPage({
+  params,
+}: {
+  params: { vaultId: string[] }
+}) {
+  const [prefix, chainId, contractAddress] = params.vaultId
+
+  return (
+    <InteractionClient
+      initialChainId={chainId}
+      initialAddress={contractAddress}
+    />
+  )
+}
